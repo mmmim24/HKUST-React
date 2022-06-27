b22583e6893e84edb7e5190413605c0dac528796
mmmim24 <mustaq24@student.sust.edu>
2022-06-27 08:55:32 +0600

    Functional MenuComponent.js

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -103,22 +103,22 @@ export const DISHES =
             {
             id: 0,
             rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
+            comment: "khabo khabo khabo",
+            author: "middleofnowhere",
             date: "2012-10-16T17:57:28.556094Z"
             },
             {
             id: 1,
             rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
+            comment: "oWo",
+            author: "MMM",
             date: "2014-09-05T17:57:28.556094Z"
             },
             {
             id: 2,
             rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
+            comment: "Oily!",
+            author: "chy",
             date: "2015-02-13T17:57:28.556094Z"
             },
             {
